cb98938930ab5f5528d45b7c27dc5eae7877d233
NicolaMGH <va3nky@hotmail.com>
2023-03-08 12:56:37 -0500

    implemented breedfetcher test

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,17 +1,20 @@
 const request = require('request');
 
-const query = process.argv.slice(2)[0];
 
-request(`https://api.thecatapi.com/v1/breeds/ddsearch?q=${query}`, (error, response, body) => {
-  console.log(error);
-  if (error !== undefined) {
-    console.log('Invalid Request', error);
-  }
-  const data = JSON.parse(body);
-  if (data.length === 0) {
-    console.log('Breed Not Found');
-  } else {
-    console.log(data[0].description);
-  }
+const fetchBreedDescription = function(breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+    if (error) {
+      callback('Invalid Request', null);
+    }
+    const data = JSON.parse(body);
+    if (data.length === 0) {
+      callback(null, 'Breed Not Found');
+    } else {
+      callback(null, data[0].description);
+    }
   
-});
+  });
+
+};
+
+module.exports = { fetchBreedDescription };
